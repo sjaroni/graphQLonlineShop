236025dc322bdd15773d54935bd2123e2469c8db
Stefan Jaroni <sjaroni@outlook.de>
2025-01-30 22:12:38 +0100

    feat: add interfaces and filter

--- src/index.js
+++ src/index.js
@@ -2,47 +2,68 @@ const { ApolloServer, gql } = require('apollo-server');
 
 const schema = gql`
   type Query {
-    products(search: String): [Product!]
-    customer: [Customer]
+    products(id: ID, search: String): [Product!]
+    customer(id: ID!): [Customer]
   }
-  type Product {
+  interface Node {
+    id: ID!
+  }
+  type Product implements Node {
+    id: ID!
     name: String!
   }
-  type Customer {    
+  type Customer implements Node {
+    id: ID!
     fullname: String
   }
 `;
 
 let productdata = [
   {
+    id: '1',
     name: 'Kartoffel',
   },
   {
+    id: '2',
     name: 'Pasta',
   },
 ];
 
 let customerdata = [
   {
-    firstname: 'Luke',
-    lastname: 'Skywalker',
+    id: '1',
+    firstname: 'Stefan',
+    lastname: 'Jaroni',
   },
 ];
 
 const resolvers = {
   Query: {
-    products: (parent, { search }) => {
+    products: (parent, { id, search }) => {
+      if (id) {
+        return productdata.filter((p) => p.id == id);
+      }
       if (search) {
         return productdata.filter((p) => p.name.includes(search));
       }
       return productdata;
     },
 
-    customer: () => {
-      return customerdata;
+    customer: (parent, { id }) => {
+      return customerdata.find((c) => c.id == id);
     },
   },
 
+  Node: {
+    __resolveType(node) {
+      if (node.toObject().name) {
+        return 'Product';
+      }
+      if (node.toObject().fullname) {
+        return 'Customer';
+      }
+    },
+  },
   Customer: {
     fullname: (customer) => `${customer.firstname} ${customer.lastname}`,
   },
